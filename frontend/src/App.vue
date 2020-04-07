ffb9c1fdac8cb0932ed50924121355e9480ab3a4
Jashpatel1 <patel.5@iitj.ac.in>
2020-04-08 01:02:29 +0530

    added basic routes example

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,11 +1,6 @@
-<template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view />
-  </div>
+<template lang="pug">
+  div(id="app")
+    router-view
 </template>
 
 <style>
@@ -16,17 +11,3 @@
   text-align: center;
   color: #2c3e50;
 }
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
