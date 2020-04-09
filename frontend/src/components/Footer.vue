7d241d5b4045e71b114669acf32b5dee40639a4e
vineethvanga18 <reddy.8@iitj.ac.in>
2020-04-09 14:49:17 +0530

    added Header and Footer

---
+++ frontend/src/components/Footer.vue
@@ -0,0 +1,17 @@
+<template lang="pug">
+  v-footer(color="#000080" padless)
+    v-row(justify="center" no-gutters)
+      v-btn(v-for="link in links" :key="link" color="white" text rounded class="py-2") {{ link }}
+    v-col(style="background-color:#000099" class="py-4 text-center white--text" cols="12") {{ new Date().getFullYear() }} — 
+        strong Devlup Labs
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      links: ["Facebook", "Instagram", "Gmail", "Github"]
+    };
+  }
+};
+</script>
---
+++ frontend/src/components/Header.vue
@@ -0,0 +1,21 @@
+<template lang="pug">
+    v-app-bar(app clipped-left color="#000080")
+        v-avatar(size="60px")
+            img(src="../assets/logo.jpeg")
+        v-toolbar-title.white--text
+            span.title.ml-3.mr-5 Devlup Labs&nbsp;
+        v-spacer
+        v-btn(color="white" text rounded class="my-2") Home
+        v-btn(color="white" text rounded class="my-2") About
+        v-btn(color="white" text rounded class="my-2") Timeline
+        v-btn(color="white" text rounded class="my-2") Blog
+        v-btn(color="white" text rounded class="my-2") Contact
+</template>
+
+<script>
+export default {
+  name: "Header"
+};
+</script>
+
+<style scoped></style>
