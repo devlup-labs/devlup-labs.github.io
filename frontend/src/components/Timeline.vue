d799036c5d46f0003d407aff41da224c0232c8b7
Sandesh Singh <sandesh1014@gmail.com>
2020-04-11 20:39:14 +0530

    minor change

--- frontend/src/components/Timeline.vue
+++ frontend/src/components/Timeline.vue
@@ -2,10 +2,10 @@
   #app
     v-app#inspire
       v-timeline(:dense='$vuetify.breakpoint.smAndDown')
-        v-timeline-item(color='light-blue' fill-dot='' center='')
+        v-timeline-item(color='light-blue' fill-dot='' right='')
           span(slot='opposite')
-            h5.display-1.light-blue--text
-              small 10-04-2020
+            p.display-1.light-blue--text
+              font(size='5') April 2020
           v-card.elevation-5(max-width='70%')
             v-card-title.light-blue
               h2.display-1.white--text.font-weight-light DevLup Labs
@@ -17,8 +17,8 @@
               v-btn.ml-5.mb-2(small='' color='light-blue' dark='') Learn More
         v-timeline-item(color='teal' fill-dot='' left='' small='')
           span(slot='opposite')
-            h5.display-1.teal--text
-              small 10-04-2020
+            p.display-1.teal--text
+              font(size='5') April 2020
           v-card.elevation-5(max-width='70%')
             v-card-title.teal.justify-end
               h2.display-1.mr-4.white--text.font-weight-light DevLup Labs
@@ -30,8 +30,8 @@
               v-btn.ml-5.mb-2(small='' color='teal' dark='') Learn More
         v-timeline-item(color='lime' fill-dot='' right='')
           span(slot='opposite')
-            h5.display-1.lime--text
-              small 10-04-2020
+            p.display-1.lime--text
+              font(size='5') April 2020
           v-card.elevation-5(max-width='70%')
             v-card-title.lime
               h2.display-1.white--text.font-weight-light DevLup Labs
@@ -43,8 +43,8 @@
               v-btn.ml-5.mb-2(small='' color='lime' dark='') Learn More
         v-timeline-item(color='orange' fill-dot='' left='' small='')
           span(slot='opposite')
-            h5.display-1.orange--text
-              small 10-04-2020
+            p.display-1.orange--text
+              font(size='5') April 2020
           v-card.elevation-5(max-width='70%')
             v-card-title.orange.justify-end
               h2.display-1.mr-4.white--text.font-weight-light DevLup Labs
