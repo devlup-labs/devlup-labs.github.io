9b1ef7451c532bc14019b40810d8018ad9045481
Sandesh1013 <57478526+Sandesh1013@users.noreply.github.com>
2020-04-13 23:29:59 +0530

    Timeline (#6)
    * change
    
    * timeline added
    
    * used pug
    
    * updated index
    
    * minor change
    
    * changes
    
    * timeline component done
    
    * made required changes
    
    * added links and changed Timelinecard to TimelineCard
    
    * converted to pug and applied prettier
    
    * title responsive
    
    * automated sides
    
    * minor changes

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -2,51 +2,51 @@ import Vue from "vue";
 import VueRouter from "vue-router";
 import Home from "../views/Home.vue";
 import Profile from "../views/Profile.vue";
+import Timeline from "../views/Timeline.vue";
 
 Vue.use(VueRouter);
 
-const routes = [
-  {
-    path: "/",
-    name: "Home",
-    component: Home
-  },
-  {
-    path: "/profile",
-    name: "Profile",
-    component: Profile
-  },
-  {
-    path: "/about",
-    name: "About",
-    component: Home
-  },
-  {
-    path: "/events",
-    name: "Events",
-    component: Profile
-  },
-  {
-    path: "/timeline",
-    name: "Timeline",
-    component: Home
-  },
-  {
-    path: "/blog",
-    name: "Blog",
-    component: Profile
-  },
-  {
-    path: "/team",
-    name: "Team",
-    component: Profile
-  }
+const routes = [{
+        path: "/",
+        name: "Home",
+        component: Home
+    },
+    {
+        path: "/profile",
+        name: "Profile",
+        component: Profile
+    },
+    {
+        path: "/about",
+        name: "About",
+        component: Home
+    },
+    {
+        path: "/events",
+        name: "Events",
+        component: Profile
+    },
+    {
+        path: "/timeline",
+        name: "Timeline",
+        component: Timeline
+    },
+    {
+        path: "/blog",
+        name: "Blog",
+        component: Profile
+    },
+    {
+        path: "/team",
+        name: "Team",
+        component: Profile
+    }
 ];
 
 const router = new VueRouter({
-  mode: "history",
-  base: process.env.BASE_URL,
-  routes
+    mode: "history",
+    base: process.env.BASE_URL,
+    routes
 });
 
 export default router;
