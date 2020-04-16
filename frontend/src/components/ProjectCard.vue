56a400620b857df4e593b21dede02688faab39bb
Jashpatel1 <patel.5@iitj.ac.in>
2020-04-16 11:40:22 +0530

    added ProjectCard

--- frontend/src/components/ProjectCard.vue
+++ frontend/src/components/ProjectCard.vue
@@ -1,32 +1,27 @@
 <template lang="pug">
-  v-card.pa-3(:min-height="getProjectHeight" :width="1.1*getProjectHeight"  border hover color="#272727")
+  v-card.pa-3(:min-height="getProjectHeight" :width="0.95*getProjectHeight"  border hover color="#272727")
     div(align="center")
       h2(align-text="center" style="color:#ffffff" underline)
-        span CDC Portal (3
-          v-icon(small) mdi-light mdi-star ) 
-          span )
-      div.pa-5 
-        v-img(src="../assets/cdclogo.jpg" contain height="150")
-      //- v-carousel(:show-arrows="false" height="200")
-      //-   v-carousel-item(v-for="(item,i) in items" contain :key="i" :src="item.src")
+        span CDC Portal 
+      v-carousel.mb-3.mt-3(hide-delimiters height="150")
+        v-carousel-item(v-for="(item,i) in items" contain :key="i" :src="item.src")
       v-divider
       div.pa-1(style="background-color:#FFFFFF")
-        h4.pa-1 A portal for handling student  placements
-        hr(color="#272727")
+        h5.pa-1 A portal for handling student  placements
+        hr
         v-flex.pa-1
-          v-chip(label dark small).pa-2.ma-1
-            v-icon(left) mdi-language-html5 
-            span HTML
-          v-chip(label dark small).pa-2.ma-1
-            v-icon(left) mdi-language-python 
-            span Python
-          v-chip(label dark small).pa-2.ma-1
-            v-icon(left) mdi-language-javascript
-            span Javascript
-        hr(color="#272727")
-        h5.pa-1 This is a web portal for automating the placement procedure. The above mentioned technology stack was used to build the project.
+              v-chip.pa-2.ma-1(label small) HTML5
+              v-chip.pa-2.ma-1(label small) Python
+              v-chip.pa-2.ma-1(label small) Javascript  
+              v-btn(icon @click="show = !show")
+                v-icon {{ show ? 'mdi-chevron-up' : 'mdi-chevron-down' }}
+        
+        v-expand-transition
+          div(v-show="show") 
+              
+            h6.pa-1 This is a web portal for automating the placement procedure. The above mentioned technology stack was used.
         hr(color="#272727" size="5px")
-        div(style="background-color:#272727" width="50px")  
+        div(style="background-color:#272727; width=50px")  
           v-card-actions.pa-0
             v-flex
               v-btn(icon href="#")
@@ -43,7 +38,10 @@
   },
   data() {
     return {
+
+      show: false,
       items: [
+        { src : require('../assets/cdclogo.jpg')},
         { src : require('../assets/cdclogo.jpg')}
       ],
     }
