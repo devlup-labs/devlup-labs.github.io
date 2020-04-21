c110b58a984ed3346fba698a1e7ab45b7c61f4b2
Dnyaneshwari Sonone <51239396+Dnyaneshwari-Sonone@users.noreply.github.com>
2020-04-22 05:29:34 +0530

    Add files via upload

---
+++ frontend/src/App.vue
@@ -0,0 +1,30 @@
+<template lang="pug">
+    v-app
+        PageHeader
+        v-content
+                router-view
+        PageFooter
+</template>
+
+
+<script>
+import PageHeader from "@/components/Header.vue";
+import PageFooter from "@/components/Footer.vue";
+
+export default {
+  name: "App",
+  components: {
+    PageHeader,
+    PageFooter
+  }
+};
+</script>
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  background-color: #747474;
+}
+</style>
