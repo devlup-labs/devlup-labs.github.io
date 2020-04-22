e673905521a337871f59a1919da90b2b034b7856
Dnyaneshwari Sonone <51239396+Dnyaneshwari-Sonone@users.noreply.github.com>
2020-04-22 18:29:03 +0530

    Added landing page (#14)
    * Add files via upload
    
    * Merge remote-tracking branch 'upstream/master' into b1
    
    * Merge remote-tracking branch 'upstream/master'
    
    * Delete App.vue
    
    * app
    
    * Add files via upload
    
    * Commit

---
+++ frontend/src/components/HomeLanding.vue
@@ -0,0 +1,44 @@
+<template lang="pug">
+div#inspire(style="height:100%;")
+  v-content.pa-0(style="height:100%;")
+    v-container.mt-12.mt-sm-0.pt-sm-3.pt-12(style='z-index:1 ; height:100%;')
+      v-row.mt-12.mt-sm-0.pt-sm-0.pt-12.px-4(align='center' justify='center' style='height:100%')
+        v-col.text-left.px-md-3.px-10(sm='7' cols='12')
+          .pl-md-10.pr-sm-7
+            h1.font-weight-bold.mb-1.mb-sm-5(:class="[$vuetify.breakpoint.lgAndUp ? 'display-4' :'display-2']") DevlUp Labs
+            h1.font-weight-regular(:class="[$vuetify.breakpoint.lgAndUp ? 'display-3' : 'display-1']")
+              span.blue--text Open Source
+              |  development enthusiasts group at IIT Jodhpur
+        v-col(sm='5' cols='12')
+
+</template>
+
+<style>
+#inspire{
+  background-image:url("../assets/bg.svg");
+  background-repeat: no-repeat;
+  background-size: 40%;
+  background-position:right top;
+}
+
+@media screen and (max-width: 1264px) and (min-width: 800px){
+  #inspire{
+      background-size: 45%;
+  }
+}
+@media screen and (max-width: 799px) and (min-width: 415px){
+  #inspire{
+      background-size: 50%;
+  }
+}
+@media (max-width:414px){
+#inspire{
+      background-size: 65%;
+  }
+}
+</style>
+
+<script>
+export default {
+};
+</script>
--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,9 +1,14 @@
 <template lang="pug">
-  v-img(alt="Devlup Labs logo" src="../assets/logo.jpeg" height="300" contain)
+  HomeLanding
 </template>
 
 <script>
+
+import HomeLanding from "../components/HomeLanding";
 export default {
-  name: "Home"
+  name: "Home",
+  components: {
+    HomeLanding
+  }
 };
 </script>
