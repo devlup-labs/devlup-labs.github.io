c8014e64994df9a517fe8f3672306c3efd9c9682
Sandesh Singh <sandesh1014@gmail.com>
2020-04-25 19:06:50 +0530

    added images

--- frontend/src/components/ProjectCard.vue
+++ frontend/src/components/ProjectCard.vue
@@ -1,29 +1,43 @@
-<template lang="pug">
-v-container
-  v-card.pa-2.elevation-6(v-for="Project in Projects" :key="Project.name" :width="0.9*getProjectHeight" hover)
-    h2(underline) {{ Project.name }}
-    v-carousel(hide-delimiters :height="0.5*getProjectHeight" show-arrows-on-hover)
-      v-carousel-item(v-for="image in Project.images" contain :key="image.src" :src="image.src")
-    v-card(flat)
-      h5.pa-1 {{ Project.description }}
-      v-divider
-      v-flex.pa-1
-        v-chip.pa-2.ma-1(v-for="link in Project.links" :key="link.name" label small) {{ link.name }}  
-        v-btn(icon @click="show = !show")
-          v-icon {{ show ? 'mdi-chevron-up' : 'mdi-chevron-down' }}
-      v-expand-transition
-        div(v-show="show") 
-          h6.pa-1 {{ Project.info }}
-      v-row.justify-space-around 
-        v-col(cols="6" v-if="Project.GithubLink")
-          v-btn.ml-1.mr-1(:href="Project.GithubLink" dark block ) 
-            v-icon mdi-github 
-        v-col(cols="6"  v-if="!Project.WebLink")  
-          v-btn(:href="Project.WebLink" block) 
-            v-icon mdi-web       
+<template>
+<v-container>
+    <v-card class="pa-2 elevation-6" v-for="Project in Projects" :key="Project.name" :width="0.9*getProjectHeight" hover="hover">
+        <h2 underline="underline">{{ Project.name }}</h2>
+        <v-carousel hide-delimiters="hide-delimiters" :height="0.5*getProjectHeight" show-arrows-on-hover="show-arrows-on-hover">
+            <v-carousel-item v-for="image in Project.images" contain="contain" :key="image.src" :src="image.src"></v-carousel-item>
+        </v-carousel>
+        <v-card flat="flat">
+            <h5 class="pa-1">{{ Project.description }}</h5>
+            <v-divider></v-divider>
+            <v-flex class="pa-1">
+                <v-chip class="pa-2 ma-1" v-for="link in Project.links" :key="link.name" label="label" small="small">{{ link.name }} </v-chip>
+                <v-btn icon="icon" @click="show = !show">
+                    <v-icon>{{ show ? 'mdi-chevron-up' : 'mdi-chevron-down' }}</v-icon>
+                </v-btn>
+            </v-flex>
+            <v-expand-transition>
+                <div v-show="show">
+                    <h6 class="pa-1">{{ Project.info }}</h6>
+                </div>
+            </v-expand-transition>
+            <v-row class="justify-space-around">
+                <v-col cols="6" v-if="Project.GithubLink">
+                    <v-btn class="ml-1 mr-1" :href="Project.GithubLink" dark="dark" block="block">
+                        <v-icon>mdi-github </v-icon>
+                    </v-btn>
+                </v-col>
+                <v-col cols="6" v-if="!Project.WebLink">
+                    <v-btn :href="Project.WebLink" block="block">
+                        <v-icon>mdi-web </v-icon>
+                    </v-btn>
+                </v-col>
+            </v-row>
+        </v-card>
+    </v-card>
+</v-container>     
 </template>
 
 <script>
+import dataProject from "../data/dataProject.json";
   export default {
     name: "ProfileCard",
   props: {
@@ -32,12 +46,7 @@ v-container
   data() {
     return {
       show: false,
-      Projects:[
-        { name: "CDC Portal", description: "A portal for handling student  placements", 
-        info: "This is a web portal for automating the placement procedure. The above mentioned technology stack was used.",
-        GithubLink: "#", WebLink: "#", links: [{ name: "python"},{ name: "html"},{ name: "javascript"}], 
-        images: [{ src : require('../assets/cdclogo.jpg')},{ src : require('../assets/Profile.png')}],}
-      ],
+      Projects: dataProject
     }
   },
   computed: {
