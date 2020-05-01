7e0393c523adb797508502f7c19590fc4c001dfe
Sandesh1013 <57478526+Sandesh1013@users.noreply.github.com>
2020-05-01 23:58:26 +0530

    Edits (#20)
    * changes
    
    * changesss
    
    * changessss
    
    * namechanged
    
    * comment removed
    
    * fixed few issues
    
    * added titles
    
    * added title
    
    * disabled horizontal scroll
    
    * chang
    
    * minor
    
    * chevron

--- src/components/TimelineCard.vue
+++ src/components/TimelineCard.vue
@@ -12,9 +12,10 @@ div
         v-row.ml-2.font-weight-bold.d-lg-none.d-xl-flex.d-xl-none {{ timelineDetail.date }}
         v-row.ml-2.mr-2.text-left {{ timelineDetail.info }}
         a(:href='timelineDetail.firstButtonLink',style='text-decoration:none;',v-if='timelineDetail.firstButtonLink')
-          v-btn.mb-2.mt-2(small='small', :color='timelineDetail.colorClass', dark='dark', v-if='timelineDetail.firstButton') {{ timelineDetail.firstButtonText }}
+          v-btn.mb-2.mt-2(small='small', style="flex-direction:column;",:color='timelineDetail.colorClass', dark='dark', v-if='timelineDetail.firstButtonLink') {{ timelineDetail.firstButtonText }}
         a(:href='timelineDetail.secondButtonLink',style='text-decoration:none;',v-if='timelineDetail.secondButtonLink')
-          v-btn.ml-2.mb-2.mt-2(small='small', :color='timelineDetail.colorClass', dark='dark', v-if='timelineDetail.secondButton') {{ timelineDetail.secondButtonText }}
+          v-btn.ml-2.mb-2.mt-2(small='small', :color='timelineDetail.colorClass', dark='dark', v-if='timelineDetail.secondButtonLink') {{ timelineDetail.secondButtonText }}
+
 </template>
 
 <script>
