0e784e4f1486c2c96c234ad9462228b07b7337bf
Sandesh Singh <sandesh1014@gmail.com>
2020-06-08 12:58:40 +0530

    16:9 viewport and reponsiveness

--- src/components/VideoCard.vue
+++ src/components/VideoCard.vue
@@ -1,11 +1,12 @@
 <template lang="pug">
 v-layout(row='' wrap='')
-  v-card.mx-auto.elevation-4(max-width='344',height='100%')
-    iframe#ytplayer(type='text/html' allowfullscreen='true' width='344' height='300' v-bind:src="`https://www.youtube.com/embed/${tutorial.guid.split(':')[2]}?autoplay=1&origin=http://example.com`" frameborder='0')
-    v-list-item
-      v-list-item-content
-        v-list-item.title {{tutorial.title}}
-        v-list-item-subtitle {{tutorial.pubDate.split(" ")[0]}}
+  v-card.mx-auto.elevation-4.d-none.d-lg-flex.d-xl-flex(max-width='500',height='100%')
+    v-responsive(:aspect-ratio='16/9')
+      iframe#ytplayer(type='text/html' allowfullscreen='true' width='500' height='100%' v-bind:src="`https://www.youtube.com/embed/${tutorial.guid.split(':')[2]}?autoplay=1&origin=http://example.com`" frameborder='0')
+  v-card.mx-auto.elevation-4.d-flex.d-sm-flex.d-md-flex.d-lg-none(max-width='344',height='100%')
+    v-responsive(:aspect-ratio='16/9')
+      iframe#ytplayer(type='text/html' allowfullscreen='true' width='344' height='100%' v-bind:src="`https://www.youtube.com/embed/${tutorial.guid.split(':')[2]}?autoplay=1&origin=http://example.com`" frameborder='0')
+    
     
 
 </template>
