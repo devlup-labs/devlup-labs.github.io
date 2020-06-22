d8032e0db25b712e638fbd460d2891a12d94ba1e
Sandesh Singh <sandesh1014@gmail.com>
2020-06-22 18:37:43 +0530

    Added Footer Links

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,15 +1,23 @@
 <template lang="pug">
     v-footer(color="basic" padless)
       v-row(justify="center" no-gutters)
-        v-btn.py-2(v-for="link in links" :key="link" color="white" icon)
-          v-icon {{link}}
+        v-btn.py-2(v-for="link in links" :key="link" :href="link.href" color="white" icon)
+          v-icon {{link.icon}}
       v-col.py-4.text-center.white--text.font-weight-bold(color="secondary" cols="12") {{ new Date().getFullYear() }} — Devlup Labs
 </template>
 <script>
 export default {
   data() {
     return {
-      links: ["mdi-gmail", "mdi-facebook", "mdi-instagram", "mdi-github"]
+      links: [
+        { icon: "mdi-gmail", href: "mailto:opensourceiitj@gmail.com" },
+        { icon: "mdi-facebook", href: "https://www.facebook.com/devluplabs/" },
+        {
+          icon: "mdi-instagram",
+          href: "https://www.instagram.com/devluplabs/"
+        },
+        { icon: "mdi-github", href: "https://github.com/devlup-labs" }
+      ]
     };
   }
 };
