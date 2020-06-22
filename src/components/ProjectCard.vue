2bc91e675a6be910bebf57dab2ba40e384786797
Sandesh Singh <sandesh1014@gmail.com>
2020-06-22 12:22:48 +0530

    project data can be fed through google sheets now

--- src/components/ProjectCard.vue
+++ src/components/ProjectCard.vue
@@ -4,7 +4,9 @@ v-row(align='center', justify='left')
     v-card.pa-2.elevation-6(:width='0.9*getProjectHeight', hover='hover')
       h2(underline='underline') {{ Project.gsx$name.$t }}
       v-carousel(hide-delimiters='hide-delimiters', :height='0.5*getProjectHeight', show-arrows-on-hover='show-arrows-on-hover')
-        v-carousel-item(v-for='image in Project.images', contain='contain', :key='image.src', :src='image.src')
+        v-carousel-item(v-if='Project.gsx$image1.$t',contain='contain', :src='Project.gsx$image1.$t')
+        v-carousel-item(v-if='Project.gsx$image2.$t',contain='contain', :src='Project.gsx$image2.$t')
+        v-carousel-item(v-if='Project.gsx$image3.$t',contain='contain', :src='Project.gsx$image3.$t')
       v-card(flat='flat')
         h5.pa-1.d-inline-blocktext-truncate(style='height:50px') {{ Project.gsx$description.$t }}
         v-divider
@@ -43,6 +45,9 @@ export default {
         this.ProjectHeight || this.$vuetify.theme.options.cards.ProjectHeight
       );
     }
+  },
+  mounted() {
+    console.log(this.Profile);
   }
 };
 </script>
