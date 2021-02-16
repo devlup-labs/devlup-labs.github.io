47cc4b1975f52ac0d2d733722b03c44cbae7afd0
venkateshjnv123 <69379565+venkateshjnv123@users.noreply.github.com>
2021-02-16 12:58:07 +0530

    Create Podcast.vue

---
+++ src/views/Podcast.vue
@@ -0,0 +1,35 @@
+<template lang="pug">
+div(align='center' ,justify='center')
+    div(v-if='loading')
+        Preloader
+    div(v-else)
+        v-container
+            v-row.mt-9
+                v-flex.mb-6(v-for='tutorial in tutorials', :key='tutorial.name' ,xs12='',sm12='',md6='',lg6='',xl4='')
+                    Podcastcard(:tutorial='tutorial')
+    </template>
+<script>
+const Podcastcard = () => import("../components/Podcastcard");
+const Preloader = () => import("../components/Preloader");
+export default {
+  name: "Podcast",
+  components: { Podcastcard, Preloader },
+  data: () => ({
+    loading: true,
+    tutorials: []
+  }),
+  methods: {
+    fetchTutorials() {
+      this.loading = true;
+      fetch(
+        "https://api.rss2json.com/v1/api.json?rss_url=https%3A%2F%2Fwww.youtube.com%2Ffeeds%2Fvideos.xml%3Fchannel_id%3DUCFaRxxB8-BB5GXH-wlwGqIw"
+      )
+        .then(e => e.json().then(e => (this.tutorials = e.items)))
+        .finally(() => (this.loading = false));
+    }
+  },
+  created() {
+    this.fetchTutorials();
+  }
+};
+</script>
