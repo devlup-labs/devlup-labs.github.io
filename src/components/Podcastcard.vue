d6282da6e67c33b252ceb7b4213a0a8f5419d372
venkateshjnv123 <69379565+venkateshjnv123@users.noreply.github.com>
2021-02-16 12:57:09 +0530

    Create Podcastcard.vue

---
+++ src/components/Podcastcard.vue
@@ -0,0 +1,15 @@
+<template lang="pug">
+v-layout(row='' wrap='')
+    v-card.mx-auto.elevation-4(:max-width='$vuetify.breakpoint.mdAndUp ? 500 : 344', height='100%')
+        v-responsive(:aspect-ratio='16/9')
+            iframe#ytplayer(type='text/html' allowfullscreen='true' width='500' height='100%' v-bind:src="`https://www.youtube.com/embed/${tutorial.guid.split(':')[2]}`" frameborder='0')
+</template>
+<script>
+export default {
+  name: "Podcastcard", 
+  props: {
+    tutorial: {type: Object , required: true}
+  }
+
+};
+</script>
