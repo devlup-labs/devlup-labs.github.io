fed59419077b3da1b996c6ab9353101e20c74b72
venkateshjnv123 <69379565+venkateshjnv123@users.noreply.github.com>
2021-02-16 19:41:29 +0530

    Update Podcastcard.vue

--- src/components/Podcastcard.vue
+++ src/components/Podcastcard.vue
@@ -1,8 +1,8 @@
 <template lang="pug">
 v-layout(row='' wrap='')
-    v-card.mx-auto(:max-width='$vuetify.breakpoint.mdAndUp ? 450 : 344', height='100%')
-        v-responsive(:aspect-ratio='16/9')
-            iframe#ytplayer(type='text/html' allowfullscreen='true' width='450' height='100%' v-bind:src="`https://www.youtube.com/embed/${podcast.guid.split(':')[2]}`" frameborder='0')
+    v-card.mx-auto(:max-width='$vuetify.breakpoint.mdAndUp ? 500 : 344', height='232px')
+        v-responsive(:aspect-ratio='22/9')
+            iframe#ytplayer(type='text/html' allowtransparency='true' width='350' height='232px' v-bind:src="`https://open.spotify.com/embed-podcast/episode/${podcast.guid.split('/')[4]}`" frameborder='0')
 </template>
 <script>
 export default {
