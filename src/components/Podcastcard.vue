b92250c091e6c2831370e262012877860f090087
venkateshjnv123 <69379565+venkateshjnv123@users.noreply.github.com>
2021-02-16 13:07:41 +0530

    Update Podcastcard.vue

--- src/components/Podcastcard.vue
+++ src/components/Podcastcard.vue
@@ -1,14 +1,14 @@
 <template lang="pug">
 v-layout(row='' wrap='')
-    v-card.mx-auto.elevation-4(:max-width='$vuetify.breakpoint.mdAndUp ? 500 : 344', height='100%')
+    v-card.mx-auto(:max-width='$vuetify.breakpoint.mdAndUp ? 450 : 344', height='100%')
         v-responsive(:aspect-ratio='16/9')
-            iframe#ytplayer(type='text/html' allowfullscreen='true' width='500' height='100%' v-bind:src="`https://www.youtube.com/embed/${tutorial.guid.split(':')[2]}`" frameborder='0')
+            iframe#ytplayer(type='text/html' allowfullscreen='true' width='450' height='100%' v-bind:src="`https://www.youtube.com/embed/${podcast.guid.split(':')[2]}`" frameborder='0')
 </template>
 <script>
 export default {
   name: "Podcastcard", 
   props: {
-    tutorial: {type: Object , required: true}
+    podcast: {type: Object , required: true}
   }
 
 };
