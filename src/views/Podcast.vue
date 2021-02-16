c2f720a8b0b2299bd12136d7b212fdbcaad9d712
venkateshjnv123 <69379565+venkateshjnv123@users.noreply.github.com>
2021-02-16 13:10:18 +0530

    Update Podcast.vue

--- src/views/Podcast.vue
+++ src/views/Podcast.vue
@@ -5,8 +5,8 @@ div(align='center' ,justify='center')
     div(v-else)
         v-container
             v-row.mt-9
-                v-flex.mb-6(v-for='tutorial in tutorials', :key='tutorial.name' ,xs12='',sm12='',md6='',lg6='',xl4='')
-                    Podcastcard(:tutorial='tutorial')
+                v-flex.mb-6(v-for='podcast in podcasts', :key='podcast.name' ,xs12='',sm12='',md6='',lg6='',xl4='')
+                    Podcastcard(:podcast='podcast')
     </template>
 <script>
 const Podcastcard = () => import("../components/Podcastcard");
@@ -16,20 +16,20 @@ export default {
   components: { Podcastcard, Preloader },
   data: () => ({
     loading: true,
-    tutorials: []
+    podcasts: []
   }),
   methods: {
-    fetchTutorials() {
+    fetchPodcast() {
       this.loading = true;
       fetch(
         "https://api.rss2json.com/v1/api.json?rss_url=https%3A%2F%2Fwww.youtube.com%2Ffeeds%2Fvideos.xml%3Fchannel_id%3DUCFaRxxB8-BB5GXH-wlwGqIw"
       )
-        .then(e => e.json().then(e => (this.tutorials = e.items)))
+        .then(e => e.json().then(e => (this.podcasts = e.items)))
         .finally(() => (this.loading = false));
     }
   },
   created() {
-    this.fetchTutorials();
+    this.fetchPodcast();
   }
 };
 </script>
