718ec2a645977dcbb7425a818012e4f7e2952d02
venkateshjnv123 <69379565+venkateshjnv123@users.noreply.github.com>
2021-02-21 09:38:24 +0530

    update footer.vue

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -4,6 +4,8 @@
       v-row(justify="center" no-gutters)
         v-btn.py-2(v-for="link in links" :key="link" :href="link.href" color="white" icon)
           v-icon {{link.icon}}
+      v-col.py-0.my-0.text-center(color='secondary' cols="12")
+       v-btn.py-0.text-center.white--text.font-weight-bold(to='./about' text='' plain='' justify='center' color='white' @click="viewform" ) Contact Us
       v-col.py-4.text-center.white--text.font-weight-bold(color="secondary" cols="12") {{ new Date().getFullYear() }} — Devlup Labs
 </template>
 <script>
@@ -22,8 +24,24 @@ export default {
           icon: "mdi-linkedin",
           href: "https://www.linkedin.com/company/devlup-labs/"
         }
-      ]
+      ],
+       clicked: false
     };
-  }
-};
+  },
+  methods: {
+    scrollform: function() {
+      if (this.clicked) {
+        window.scrollTo(0, document.body.scrollHeight);
+        this.clicked = false ;
+      }
+    },
+    viewform: function() {
+      this.clicked = true;
+        this.scrollform();
+    }
+  },
+  components: {
+    form: form
+} };
 </script>
+
