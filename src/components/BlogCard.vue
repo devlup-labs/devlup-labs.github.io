a4d02176f91507ebc5ae1fb38df56539101d66ea
Yuvraj Rathva <rathva.1@iitj.ac.in>
2022-07-21 15:05:48 +0530

    clickeffect and size of tags

--- src/components/BlogCard.vue
+++ src/components/BlogCard.vue
@@ -6,23 +6,26 @@
   -webkit-box-orient: vertical;
   -webkit-line-clamp: 3;
 }
+.cardclicked:before {
+  background: none;
+}
 </style>
 <template lang="pug">
 v-layout(row='' wrap='')
-  v-card.mx-auto.elevation-4.pa-md-2(max-width='344',height='370',:href="blog.link",target='_blank')
+  v-card.mx-auto.elevation-4.pa-md-2.cardclicked(v-ripple="false" max-width='344',height='370',:href="blog.link",target='_blank')
     v-img(:src="blog.thumbnail" height='150')
-    v-container.py-1
+    v-container.py-2
     v-list-item
       v-list-item-content
         v-list-item.title.px-md-2.elip {{blog.title}}
         v-list-item-subtitle.mb-n4.mt-1 {{blog.author}}
     v-card-actions
         v-col
-          v-row.align-content-center.justify-center
+          v-row.justify-center
             v-sheet(max-width='100%' @click.prevent="add") 
                 v-slide-group(multiple='' show-arrows='')
-                  v-slide-item(v-for='tags in blog.categories' :key='tags.name' small='small' v-slot='{ active, toggle }')
-                    v-btn.mx-0(:input-value='activenp' :ripple="false" active-class='black white--text' depressed='' rounded='' @click='toggle')
+                  v-slide-item.caption(v-for='tags in blog.categories' :key='tags.name' small='small' v-slot='{ active, toggle }')
+                    v-btn.text-lowercase(:input-value='activenp' height="80%" active-class='black white--text' depressed='' rounded="" @click='toggle')
                       |{{ tags }}
                     
 
